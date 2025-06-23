a383f33d18d6eac7f1c0d1fa7d3db0aedea840ab
Satyarthranjan8051 <SatyarthRanjan4488@gmail.com>
2025-06-23 15:11:33 +0530

    this is my first portfolio website

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar from "./sections/navbar.jsx";
+const App = () => {
+  return (
+    <div className="container mx-auto max-w-7xl">
+      <Navbar />
+      {/* hero */}
+      {/* about */}
+      {/* projects */}
+      {/* experience */}
+      {/* contact */}
+      {/* footer */}
+    </div>
+  );
+};
+
+export default App;
