3dd08b85c4aaebfc3ebd1d648645ba68a4151e0e
Satyarthranjan8051 <SatyarthRanjan4488@gmail.com>
2025-06-24 23:37:13 +0530

    update

--- src/sections/navbar.jsx
+++ src/sections/navbar.jsx
@@ -1,6 +1,15 @@
 const Navbar = () => {
   return (
-    <div className="fixed inset-x-0 z-20 w-full backdrop:">Navbar</div>
+    <span>
+      <div className="fixed inset-x-0 z-20 w-full backdrop:">Projects</div>
+      <div className="fixed inset-x-0 z-20 w-full backdrop:">about me</div>
+      <div className="fixed inset-x-0 z-20 w-full backdrop:">experience</div>
+      <div className="fixed inset-x-0 z-20 w-full backdrop:">achievements</div>
+      <div className="fixed inset-x-0 z-20 w-full backdrop:">certificates</div>
+
+    </span>
+    
+    
   )
 }
 
